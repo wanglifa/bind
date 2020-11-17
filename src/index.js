6426bb2a0e9ea5deee1122dce53dd58284e78853
wanglifa <wanglifa1995@gmail.com>
2020-11-17 16:47:57 +0800

    实现new

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 function _bind(asThis, ...args) {
     const fn = this
-    return function(...args2) {
-        return fn.call(asThis, ...args, ...args2)
+    function resultFn(...args2) {
+        // var temp = {}
+        // this = temp
+        // this.__proto__ = resultFn.prototype
+        return fn.call(this instanceof resultFn ? this : asThis, ...args, ...args2)
     }
+    resultFn.prototype = fn.prototype
+    return resultFn
 }
 var slice = Array.prototype.slice
 function bind(asThis) {
@@ -11,10 +16,13 @@ function bind(asThis) {
     if (typeof fn !== 'function') {
         throw new Error('bind的类型必须是函数')
     }
-    return function() {
+    function resultFn() {
         var args2 = slice.call(arguments, 0)
-        return fn.apply(asThis, args.concat(args2))
+        return fn.apply(this instanceof resultFn ? this : asThis, args.concat(args2))
     }
+    resultFn.prototype = fn.prototype
+    return resultFn
+
 }
 module.exports = bind
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
 const bind = require('../src/index')
 
+test1('fn.bind能用')
+test2('this 绑定成功')
+test3('this, p1, p2 绑定成功')
+test4('this, pq 绑定成功, 后传 p2 调用成功')
+test5('new 的时候绑定p1, p2')
+test6('new 的时候绑定p1, p2,并且实例对象可以使用构造函数.prototype上的方法')
+
 function test1(message) {
     console.log(message)
     Function.prototype.bind2 = bind
@@ -36,8 +43,34 @@ function test4(message) {
     console.assert(anotherFn2(222, 333)[1] === 222)
     console.assert(anotherFn2(222, 333)[2] === 333)
 }
+function test5(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn = function(p1, p2) {
+        this.p1 = p1
+        this.p2 = p2
+    }
+    const fn2 = fn.bind2(undefined, 'x', 'y')
+    const object = new fn2()
+    console.log(object, 'o')
+    console.assert(object.p1 === 'x')
+    console.assert(object.p2 === 'y')
+}
+function test6(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn = function(p1, p2) {
+        this.p1 = p1
+        this.p2 = p2
+    }
+    fn.prototype.say = function() {
+        console.log('say')
+    }
+    const fn2 = fn.bind2(undefined, 'x', 'y')
+    const object = new fn2()
+    console.assert(object.p1 === 'x')
+    console.assert(object.p2 === 'y')
+    console.assert(object.__proto__ === fn.prototype)
+    console.assert(typeof object.say === 'function')
+}
 
-test1('fn.bind能用')
-test2('this 绑定成功')
-test3('this, p1, p2 绑定成功')
-test4('this, pq 绑定成功, 后传 p2 调用成功')
