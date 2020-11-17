66604ca39fa83ab63e33182243979498512849d9
wanglifa <wanglifa1995@gmail.com>
2020-11-17 14:59:46 +0800

    封装test

--- test/index.js
+++ test/index.js
@@ -1,22 +1,43 @@
 const bind = require('../src/index')
-Function.prototype.bind2 = bind
-console.assert(Function.prototype.bind2 !== undefined)
 
-const fn1 = function() {
-    return this
+function test1(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    console.assert(Function.prototype.bind2 !== undefined)
 }
-const newFn1 = fn1.bind2({ name: 'lifa' })
-console.assert(newFn1().name === 'lifa')
-
-const fn2 = function(p1, p2) {
-    return [this, p1, p2]
+function test2(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn1 = function() {
+        return this
+    }
+    const newFn1 = fn1.bind2({ name: 'lifa' })
+    console.assert(newFn1().name === 'lifa')
 }
-const newFn2 = fn2.bind2({ name: 'lifa' }, 123, 456)
-console.assert(newFn2()[0].name === 'lifa')
-console.assert(newFn2()[1] === 123)
-console.assert(newFn2()[2] === 456)
+function test3(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn2 = function(p1, p2) {
+        return [this, p1, p2]
+    }
+    const newFn2 = fn2.bind2({ name: 'lifa' }, 123, 456)
+    console.assert(newFn2()[0].name === 'lifa')
+    console.assert(newFn2()[1] === 123)
+    console.assert(newFn2()[2] === 456)
+}
+function test4(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn2 = function(p1, p2) {
+        return [this, p1, p2]
+    }
+    const anotherFn2 = fn2.bind2({ name: 'lifa' })
+    console.assert(anotherFn2(222, 333)[0].name === 'lifa')
+    console.assert(anotherFn2(222, 333)[1] === 222)
+    console.assert(anotherFn2(222, 333)[2] === 333)
+}
 
-const anotherFn2 = fn2.bind2({ name: 'lifa' })
-console.assert(anotherFn2(222, 333)[0].name === 'lifa')
-console.assert(anotherFn2(222, 333)[1] === 222)
-console.assert(anotherFn2(222, 333)[2] === 333)
+test1('fn.bind能用')
+test2('this 绑定成功')
+test3('this, p1, p2 绑定成功')
+test4('this, pq 绑定成功, 后传 p2 调用成功')
